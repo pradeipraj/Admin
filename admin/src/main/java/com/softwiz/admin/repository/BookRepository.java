eeb996e02902f67caffa8725573bc18b57e04d5d
Pradeep Raj <129373542+pradeipraj@users.noreply.github.com>
2023-09-03 22:21:45 +0530

    Delete admin directory

--- admin/src/main/java/com/softwiz/admin/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.softwiz.admin.repository;
-
-import com.softwiz.admin.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-
-}
