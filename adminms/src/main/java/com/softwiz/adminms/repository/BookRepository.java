63f593a3288506e497f47d8ad3e99a40e5c9e355
Pradeep Raj <129373542+pradeipraj@users.noreply.github.com>
2023-09-03 22:22:00 +0530

    Delete adminms directory

--- adminms/src/main/java/com/softwiz/adminms/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.softwiz.adminms.repository;
-
-import com.softwiz.adminms.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-
-}
